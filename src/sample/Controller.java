9e5eb54ebd0bd393d1a945d838ef3a5b1436c2d8
Pablo Marcos Sánchez <pablomarcossanche06.pm@gmail.com>
2021-02-21 13:00:25 +0100

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,27 @@
+package sample;
+
+
+import java.awt.*;
+
+public class Controller {
+
+    public TextField texto1;
+
+    public TextField texto2;
+
+    public TextField pokemon1;
+
+    public TextField Pokemon2;
+
+    public TextField Pokemon3;
+
+    public TextField Pokemon4;
+
+    public TextField Pokemon5;
+
+    public TextField Pokemon6;
+
+
+}
+
+
