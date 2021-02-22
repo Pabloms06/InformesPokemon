e90d7666849ee220d5a07bab0af72b0b83d7eab0
Pablo Marcos Sánchez <pablomarcossanche06.pm@gmail.com>
2021-02-22 18:40:50 +0100

    Actualizado

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,5 +1,9 @@
 package sample;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.chart.PieChart;
 
 import java.awt.*;
 
@@ -22,6 +26,31 @@ public class Controller {
     public TextField Pokemon6;
 
 
-}
+
+        @FXML
+        public void PulsarBoton(ObservableList<PieChart.Data> data){
+            PieChart pie = new PieChart(data);
+
+            data = FXCollections.observableArrayList();
+            String uno = pokemon1.getText();
+            String dos = Pokemon2.getText();
+            String tres = Pokemon3.getText();
+            String cuatro = Pokemon4.getText();
+            String cinco = Pokemon5.getText();
+            String seis = Pokemon6.getText();
+
+
+            new PieChart.Data(uno,1);
+            new PieChart.Data(dos,2);
+            new PieChart.Data(tres,3);
+            new PieChart.Data(cuatro,4);
+            new PieChart.Data(cinco,5);
+            new PieChart.Data(seis,6);
+
+            pie.setTitle(texto1.getText());
+            pie.setLabelLineLength(Double.parseDouble(texto2.getText()));
+        }
+
+    }
 
 
